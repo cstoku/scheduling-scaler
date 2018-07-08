29d8d29ce87baac2ef76a67d2b0671ccdd45913a
Takuya Tokuda <cs.toku.mail@gmail.com>
2018-07-08 12:47:39 +0900

    データ型更新

--- pkg/apis/apps/v1alpha1/types.go
+++ pkg/apis/apps/v1alpha1/types.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	"time"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -15,11 +17,17 @@ type SchedulingScaler struct {
 }
 
 type SchedulingScalerStatus struct {
-	Name string `json:"name"`
+	ScheduledReplicas int32 `json:"scheduledReplicas"`
 }
 
 type SchedulingScalerSpec struct {
-	Name string `json:"name"`
+	Schedules      []SchedulingScalerSchedule  `json:"schedules"`
+	ScaleTargetRef CrossVersionObjectReference `json:"scaleTargetRef"`
+}
+
+type SchedulingScalerSchedule struct {
+	ScheduleTime SchedulingTime `json:"scheduleTime"`
+	Replicas     int32          `json:"replicas"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -28,4 +36,31 @@ type SchedulingScalerList struct {
 	metav1.ListMeta `json:"metadata"`
 
 	Items []SchedulingScaler `json:"items"`
+}
+
+type CrossVersionObjectReference struct {
+	Kind       string `json:"kind"`
+	Name       string `json:"name"`
+	APIVersion string `json:"apiVersion"`
+}
+
+type SchedulingTime struct {
+	time.Time
+}
+
+func (st SchedulingTime) format() string {
+	return st.Time.Format("15:04")
+}
+
+func (st SchedulingTime) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + st.format() + `"`), nil
+}
+
+func (st *SchedulingTime) UnmarshalJSON(data []byte) (err error) {
+	t, err := time.Parse("15:04", string(data))
+	if err != nil {
+		return err
+	}
+	st = &SchedulingTime{t}
+	return
 }
