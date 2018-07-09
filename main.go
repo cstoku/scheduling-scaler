86e9bc240f61d66f160ad1c0cca870809c95723e
Takuya Tokuda <cs.toku.mail@gmail.com>
2018-07-09 12:24:56 +0900

    フラグ処理の追加

--- main.go
+++ main.go
@@ -74,3 +74,8 @@ func main() {
 		glog.Fatalf("Error running controller: %s", err.Error())
 	}
 }
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
